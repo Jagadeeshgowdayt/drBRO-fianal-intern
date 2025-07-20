a9fe91656ccf5a71796b8dce48042c6788ccf45f
Jagadeesh S <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2025-07-21 01:15:10 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -606,7 +606,7 @@ async def log_error(client, error_message):
     try:
         await client.send_message(
             chat_id=LOG_CHANNEL, 
-            text=f"<b>⚠️ udf -fs set :</b>
+            text=f"<b>⚠️ udf -fs set :</b>"
         )
     except Exception as e:
         print(f"Failed to log error: {e}")
