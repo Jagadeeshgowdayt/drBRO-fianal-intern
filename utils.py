0d6047c33d959114ea803545385fa635b98e55c6
Jagadeesh S <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2025-07-21 01:09:40 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -606,7 +606,7 @@ async def log_error(client, error_message):
     try:
         await client.send_message(
             chat_id=LOG_CHANNEL, 
-            text=f"<b>⚠️ user downloaded file ...also set fast @jagadeesh :</b>\n<code>{error_message}</code>"
+            text=f"<b>⚠️ udf -fs set :</b>
         )
     except Exception as e:
         print(f"Failed to log error: {e}")
