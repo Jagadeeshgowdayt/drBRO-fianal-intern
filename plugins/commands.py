52325f80ff00cee0ecbcc96dec3a28b64e30306c
Jagadeesh S <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2025-07-25 00:07:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -99,7 +99,7 @@ async def start(client, message):
     if len(message.command) != 2:
         buttons = [
     [
-        InlineKeyboardButton("🎬 Send your favorite movie name", url="https://t.me/DrBerlin_bot")
+        InlineKeyboardButton("Explore Movies 🎬", callback_data="explore_start")
     ]
 ]
         reply_markup = InlineKeyboardMarkup(buttons)
